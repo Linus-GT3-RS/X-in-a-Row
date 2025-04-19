a0d20aeb0e43aae8282a1db9051f5a040969ccf2
dotex <dotex@PC-Linus>
2025-04-19 11:18:49 +0200

    removed port and id logic from player class

--- src/lab1/game/Player.java
+++ src/lab1/game/Player.java
@@ -2,49 +2,36 @@ package lab1.game;
 
 import java.awt.Color;
 
-public class Player {
-	
-	private int port;
+public class Player {	
 	
 	private String name;
-	private Color color;
-	
+	private Color color;	
 	private int points;
 	
-	public Player(int port, String name) {
-		super();
-		this.port = port;
+	public Player(String name) {
 		this.name = name;
 		this.color = calcColor(name);
 		this.points = 0;
 	}
 	
 	public static Color calcColor(String name) {
-        int hash = name.hashCode();
-        
-        int red = (hash >> 16) & 0xFF;   // Die oberen 8 Bits für Rot
-        int green = (hash >> 8) & 0xFF;  // Die mittleren 8 Bits für Grün
-        int blue = hash & 0xFF;          // Die unteren 8 Bits für Blau
-
+        int hash = name.hashCode();        
+        int red = (hash >> 16) & 0xFF;   
+        int green = (hash >> 8) & 0xFF;  
+        int blue = hash & 0xFF;         
         return new Color(red, green, blue);
 	}
-	
-	
-	public int getPort() {
-		return this.port;
-	}
-	
+		
 	public Color getColor() {
 		return this.color;
 	}
-	
-	// can be from 0-9
-	public int getID() {
-		return port - 90000;
-	}
-	
+		
 	public int getPoints() {
 		return points;
+	}
+	
+	public String getName() {
+		return name;
 	}
 	
 	public Player incrPoints() {
@@ -56,7 +43,5 @@ public class Player {
 		points = p;
 	}
 	
-	public String getName() {
-		return name;
-	}
+	
 }
